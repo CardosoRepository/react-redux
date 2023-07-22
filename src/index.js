bf4ff8f8592a3d2ac533510bdd00de3a388dbec2
Cardoso <gabrielcard0s0@hotmail.com>
2023-07-22 20:20:35 -0300

    Aula 17: Componente com parâmetro

--- src/index.js
+++ src/index.js
@@ -2,10 +2,19 @@ import './index.css'
 import ReactDOM from 'react-dom'
 import React from 'react'
 import Primeiro from './components/basics/Primeiro'
+import ComParametro from './components/basics/ComParametro'
 
 ReactDOM.render(
-    <div>
+    <div id="app">
         <Primeiro></Primeiro>
+        <ComParametro 
+            titulo="Situação do aluno" 
+            aluno="Pedro"
+            nota={6.3}/>
+        <ComParametro 
+            titulo="Situação da aluna" 
+            aluno="Maria"
+            nota={9.9}/>
     </div>,
     document.getElementById('root')
 )
