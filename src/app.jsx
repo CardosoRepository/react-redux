de461689558930d838e9624a3c32c007f2fa5d41
Cardoso <gabrielcard0s0@hotmail.com>
2023-07-23 13:02:00 -0300

    aula 23: cards

--- src/app.jsx
+++ src/app.jsx
@@ -3,15 +3,24 @@ import React from "react";
 import Primeiro from "./components/basics/Primeiro";
 import ComParametro from "./components/basics/ComParametro";
 import Fragmento from "./components/basics/Fragmento";
-import Aleatorio from "./components/basics/Aleatorio"
+import Aleatorio from "./components/basics/Aleatorio";
+import Card from "./components/layout/Card";
 
 export default (_) => (
     <div id="app">
         <h1>Fundamentos React</h1>
-        <Aleatorio min={50} max={80}></Aleatorio>
-        <Primeiro></Primeiro>
-        <ComParametro titulo="Situação do aluno" aluno="Pedro" nota={6.3} />
-        <ComParametro titulo="Situação da aluna" aluno="Maria" nota={9.9} />
-        <Fragmento />
+        <Card titulo="#01 - Primeiro Exercício">
+            <Primeiro></Primeiro>
+        </Card>
+        <Card titulo="#02 - Com Parâmetro">
+            <ComParametro titulo="Situação do aluno" aluno="Pedro" nota={9.3} />
+        </Card>
+        <Card titulo="#03 - Fragmento">
+            <Fragmento />
+        </Card>
+        <Card titulo="#04 - Desafio aleatório">
+            <Aleatorio min={1} max={60}></Aleatorio>
+        </Card>
+        
     </div>
 );
