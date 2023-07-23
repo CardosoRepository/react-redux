b162545c81a0455c9e5380366a93b8821f3f4f5d
Cardoso <gabrielcard0s0@hotmail.com>
2023-07-23 13:46:11 -0300

    aula 27: passagem de parâmetro entre componentes

--- src/app.jsx
+++ src/app.jsx
@@ -1,11 +1,13 @@
 import React from "react";
-import './App.css';
+import "./App.css";
 
 import Primeiro from "./components/basics/Primeiro";
 import ComParametro from "./components/basics/ComParametro";
 import Fragmento from "./components/basics/Fragmento";
 import Aleatorio from "./components/basics/Aleatorio";
 import Card from "./components/layout/Card";
+import Family from "./components/basics/Family";
+import FamilyMember from "./components/basics/FamilyMember";
 
 export default (_) => (
     <div className="App">
@@ -15,13 +17,24 @@ export default (_) => (
                 <Primeiro></Primeiro>
             </Card>
             <Card title="#02 - Com Parâmetro" color="#E8B71A">
-                <ComParametro title="Situação do aluno" student="Pedro" grade={9.3} />
+                <ComParametro
+                    title="Situação do aluno"
+                    student="Pedro"
+                    grade={9.3}
+                />
             </Card>
             <Card title="#03 - Fragmento" color="#E94C6F">
                 <Fragmento />
             </Card>
             <Card title="#04 - Desafio aleatório" color="#FA6900">
-                <Aleatorio min={1} max={60}></Aleatorio>
+                <Aleatorio min={1} max={60} />
+            </Card>
+            <Card title="#05 - Componente com filhos" color="#00C8F8">
+                <Family surname="Silva" >
+                    <FamilyMember name="Pedro" />
+                    <FamilyMember name="Ana"  />
+                    <FamilyMember name="Gustavo" />
+                </Family>
             </Card>
         </div>
     </div>
