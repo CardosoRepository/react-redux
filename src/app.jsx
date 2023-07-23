9593b03b8ceaf8bbba3458d4d13a358b861093b3
Cardoso <gabrielcard0s0@hotmail.com>
2023-07-23 13:11:33 -0300

    aula 24: estilização do display dos cards

--- src/app.jsx
+++ src/app.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './App.css';
 
 import Primeiro from "./components/basics/Primeiro";
 import ComParametro from "./components/basics/ComParametro";
@@ -7,20 +8,21 @@ import Aleatorio from "./components/basics/Aleatorio";
 import Card from "./components/layout/Card";
 
 export default (_) => (
-    <div id="app">
+    <div className="App">
         <h1>Fundamentos React</h1>
-        <Card titulo="#01 - Primeiro Exercício">
-            <Primeiro></Primeiro>
-        </Card>
-        <Card titulo="#02 - Com Parâmetro">
-            <ComParametro titulo="Situação do aluno" aluno="Pedro" nota={9.3} />
-        </Card>
-        <Card titulo="#03 - Fragmento">
-            <Fragmento />
-        </Card>
-        <Card titulo="#04 - Desafio aleatório">
-            <Aleatorio min={1} max={60}></Aleatorio>
-        </Card>
-        
+        <div className="Cards">
+            <Card titulo="#01 - Primeiro Exercício">
+                <Primeiro></Primeiro>
+            </Card>
+            <Card titulo="#02 - Com Parâmetro">
+                <ComParametro titulo="Situação do aluno" aluno="Pedro" nota={9.3} />
+            </Card>
+            <Card titulo="#03 - Fragmento">
+                <Fragmento />
+            </Card>
+            <Card titulo="#04 - Desafio aleatório">
+                <Aleatorio min={1} max={60}></Aleatorio>
+            </Card>
+        </div>
     </div>
 );
